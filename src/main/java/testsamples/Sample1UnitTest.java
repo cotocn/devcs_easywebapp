a6a6bf7923bf7da069fb815c9d7050ee223777f8
nobuhikosekiya <nobuhikosekiya@users.noreply.github.com>
2016-09-15 16:50:26 +0900

    Added junit testcases

---
+++ src/main/java/testsamples/Sample1UnitTest.java
@@ -0,0 +1,24 @@
+package testsamples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Sample1UnitTest {
+
+	@Test
+	public void test1() {
+		assertTrue("test1 is working", true);
+	}
+
+	@Test
+	public void test2() {
+		assertTrue("test2 is working", true);
+	}
+	
+	@Test
+	public void test3() {
+		assertTrue("test3 is working", true);
+	}
+	
+}
